f1311a50bcb54fb5c3f175b87de73802bb305237
srujanjoshi <srujanjoshi7@gmail.com>
2022-02-25 22:53:07 -0500

    Display Movie Titles

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -7,11 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+        
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
+                    print(self.movies[0])
+                    // TODO: Reload your table view data
+                    self.tableView.reloadData()
+             }
+        }
+        task.resume()
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        cell.textLabel!.text = title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
 
 
